ba3cb58c75c5011a826d5c13ecc0aa83b462e9c7
hanzhouyang <651272115@qq.com>
2019-08-02 20:31:06 +0800

    使用element-ui和Vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
-    APP
+    <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
 <script>
 
-
 export default {
   name: 'app'
-  
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// import router from './router/index.js'
+import router from '@/router'
+import VueRouter from 'vue-router'
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+Vue.use(VueRouter)
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
